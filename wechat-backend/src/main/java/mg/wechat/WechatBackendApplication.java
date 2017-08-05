7ab40136604413d7c71267ed47c57ff86860c15a
ratombotsoam <ratombotsoam@DCDMC-LAP-063.DCDMC.LOCAL>
2017-08-05 22:21:00 +0400

    Backend application scaffolding

---
+++ wechat-backend/src/main/java/mg/wechat/WechatBackendApplication.java
@@ -0,0 +1,12 @@
+package mg.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatBackendApplication.class, args);
+	}
+}
---
+++ wechat-backend/src/test/java/mg/wechat/WechatBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package mg.wechat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
