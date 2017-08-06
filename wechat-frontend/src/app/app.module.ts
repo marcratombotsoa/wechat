111ae7125514de642f499535f1dfbf085fa75d15
ratombotsoam <ratombotsoam@DCDMC-LAP-063.DCDMC.LOCAL>
2017-08-06 09:17:34 +0400

    Added angular material

--- wechat-frontend/src/app/app.module.ts
+++ wechat-frontend/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, 
+    MdCardModule, 
+    MdMenuModule, 
+    MdToolbarModule, 
+    MdIconModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +19,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
