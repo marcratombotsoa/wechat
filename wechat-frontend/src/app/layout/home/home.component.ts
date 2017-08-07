47898834f47f5963d11baa926720e510a0b4f9dc
ratombotsoam <ratombotsoam@DCDMC-LAP-063.DCDMC.LOCAL>
2017-08-07 10:05:05 +0400

    Snack bar notification on new message + UI

--- wechat-frontend/src/app/layout/home/home.component.ts
+++ wechat-frontend/src/app/layout/home/home.component.ts
@@ -8,6 +8,7 @@ import { Message } from '../../shared/model/message';
 import { StompService } from 'ng2-stomp-service';
 import { ChannelUtil } from '../../shared/service/channel-util';
 import { settings } from '../../shared/util/settings';
+import { MdSnackBar } from '@angular/material';
 
 @Component( {
     selector: 'app-home',
@@ -25,13 +26,13 @@ export class HomeComponent implements OnInit {
     private channel: string;
 
     constructor( private router: Router, private userService: UserService
-            , private stompService: StompService ) {
+            , private stompService: StompService, private snackBar: MdSnackBar ) {
         stompService.configure({
             host: settings.baseUrl + '/wechat',
             queue: {'init': false}
         });
     }
-
+    
     ngOnInit() {
         this.messages = [];
         this.username = sessionStorage.getItem( "user" );
@@ -59,9 +60,14 @@ export class HomeComponent implements OnInit {
                });
                
                this.stompService.subscribe('/channel/logout', res => {
-                   this.users = this.users.filter(function (item) {
-                       return item.username !== res.username;
-                   });
+                   this.users = this.users.filter(item => item.username !== res.username);
+                   const channelId = ChannelUtil.createChannel(this.username, res.username);
+                   if (this.channel === channelId) {
+                       this.messages = this.messages.filter(message => message.channel !== channelId);
+                       this.filterMessages();
+                       this.receiver = null;
+                       this.channel = null;
+                   }
                });
                
                this.subscribeToOtherUsers(this.users, this.username);
@@ -78,6 +84,10 @@ export class HomeComponent implements OnInit {
         this.stompService.subscribe('/channel/chat/' + channelId, res => {
             this.messages.push(res);
             this.filterMessages();
+            
+            if (res.channel !== this.channel) {
+                this.showNotification(res);
+            }
         });
         
         return channelId;
@@ -93,6 +103,9 @@ export class HomeComponent implements OnInit {
         this.stompService.send('/app/messages', {'channel': this.channel
             , 'sender': this.username, 'content': this.newMessage});
         this.newMessage = '';
+        
+        var msgContainer = document.getElementById("msg-container");
+        msgContainer.scrollTop = msgContainer.scrollHeight;
     }
 
     filterMessages() {
@@ -115,5 +128,16 @@ export class HomeComponent implements OnInit {
     
     getOtherUsers(): Array<User> {
         return this.users.filter(user => user.username !== this.username);
+    }
+    
+    showNotification(message: Message) {
+        let snackBarRef = this.snackBar.open('New message from ' + message.sender, 'Show');
+        
+        snackBarRef.onAction().subscribe(() => {
+            console.log('start chat with ' + message.sender);
+            this.receiver = message.sender;
+            this.channel = ChannelUtil.createChannel(this.username, message.sender);
+            this.filterMessages();
+          });
     }
 }
--- wechat-frontend/src/app/layout/home/home.module.ts
+++ wechat-frontend/src/app/layout/home/home.module.ts
@@ -8,7 +8,8 @@ import {
     MdInputModule,
     MdButtonModule,
     MdListModule,
-    MdIconModule
+    MdIconModule,
+    MdSnackBarModule
 } from '@angular/material';
 
 @NgModule({
@@ -20,7 +21,8 @@ import {
     MdInputModule,
     MdButtonModule,
     MdListModule,
-    MdIconModule
+    MdIconModule,
+    MdSnackBarModule
   ],
   declarations: [HomeComponent]
 })
